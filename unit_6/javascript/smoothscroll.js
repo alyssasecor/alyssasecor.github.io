28bfea05d5095ea72ca25e82ddf72189bf1eb471
alyssasecor.github.io <alyssasecor@gmail.com>
2017-01-31 16:44:54 -0600

    Upload

--- unit_6/javascript/smoothscroll.js
+++ unit_6/javascript/smoothscroll.js
@@ -6,7 +6,7 @@
  * Date: 10-Sep-2012
  * Version: 1.0.1
  */
-if (!window['jQuery']) alert('The jQuery library must be included before the smoothscroll.js file.  The plugin will not work propery.');
+if (!window['jQuery']) ;
 
 /**
  * jQuery.ScrollTo - Easy element scrolling using jQuery.
